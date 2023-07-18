c498759bc3ba68ae3a6dedb2502726237cd0cc21
Ellyboi <vitoesitemitope@gmail.com>
2023-07-18 17:12:51 +0100

    add BlogApp controller rspec

---
+++ spec/posts_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'PostsController', type: :request do
+  describe 'GET #index' do
+    it 'returns http success' do
+      get user_posts_path(user_id: 1)
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get user_post_path(user_id: 1, id: 1)
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+    end
+  end
+end
---
+++ spec/users_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'UsersController', type: :request do
+  describe 'GET #index' do
+    it 'returns http success' do
+      get users_path
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get user_path(1)
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+    end
+  end
+end
