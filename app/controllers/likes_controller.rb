b9f9b6526c4436226781c77f7bf364191f646e04
Ellyboi <vitoesitemitope@gmail.com>
2023-07-21 09:53:57 +0100

    still on linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,13 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    
-    # Check if the user has already liked the post
-    if @post.likes.where(user: current_user).exists?
+
+    if @post.likes.exists?(user: current_user)
       redirect_to user_post_path(user_id: params[:user_id], id: @post.id), alert: 'You have already liked this post.'
     else
       @like = Like.new(post: @post, user: current_user)
 
       if @like.save
-        # Increment likes_counter in the post
         @post.increment!(:likes_counter)
         redirect_to user_post_path(user_id: params[:user_id], id: @post.id), notice: 'You liked this post!'
       else
