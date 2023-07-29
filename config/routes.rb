ebbf1a730b1558d70449c2c806d80d334165ee8e
Ellyboi <vitoesitemitope@gmail.com>
2023-07-29 17:51:43 +0100

    add api

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:create, :destroy]
       resource :likes, only: [:create, :destroy]
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index]
+      end
+
+      resources :posts, only: [] do
+        resources :comments, only: [:index, :create]
+      end
     end
   end
 end
